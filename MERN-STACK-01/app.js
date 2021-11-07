7acd934552d1314dc459c0a8ea989559670ae223
Areeb Bin Azim <areebbinazim360@gmail.com>
2021-11-07 18:23:33 +0500

    mern-stack-01 initial files
    Creating servers | Middleware | Connect to MongoDb Atlas

---
+++ MERN-STACK-01/app.js
@@ -0,0 +1,50 @@
+const dotenv = require('dotenv')
+const mongoose = require('mongoose');
+const express= require('express');
+const app = express()
+
+dotenv.config({path:"./config.env"})
+
+
+// Database connection
+require('./DB/connectDb')
+
+const PORT = process.env.PORT;
+
+
+const User = require('./models/userSchema')
+
+
+
+
+// MIDDLEWARE
+
+const middleware=(req,res,next)=>{
+    console.log("THIS IS A about PAGE")
+    next()
+}
+
+
+app.get('/', (req, res)=>{
+    res.send("THIS IS A HOME PAGE")
+})
+
+app.get('/about', middleware,(req, res)=>{
+    res.send("THIS IS A about PAGE")
+})
+
+app.get('/contact', (req, res)=>{
+    res.send("THIS IS A contact PAGE")
+})
+
+app.get('/signin', (req, res)=>{
+    res.send("THIS IS A signin PAGE")
+})
+
+app.get('/signup', (req, res)=>{
+    res.send("THIS IS A signup PAGE")
+})
+
+app.listen(PORT,()=>{
+    console.log(`Successfully running server on port ${PORT}`)
+})
